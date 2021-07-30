2f1448d2a499b8cf0ed59b45cb9453ceef90bfbc
wbf <274904168@qq.com>
2021-07-30 17:56:34 +0800

    edit

---
+++ spring-boot-logging-demo/src/main/java/cn/javanorth/springbootloggingdemo/SpringBootLoggingDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.javanorth.springbootloggingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoggingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingDemoApplication.class, args);
+    }
+
+}
---
+++ spring-boot-logging-demo/src/test/java/cn/javanorth/springbootloggingdemo/SpringBootLoggingDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.javanorth.springbootloggingdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoggingDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
