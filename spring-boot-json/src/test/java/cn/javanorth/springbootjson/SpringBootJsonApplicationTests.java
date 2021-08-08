f9a47bb9744dc698a0ebf0f4b06efdcca937048f
wbf <274904168@qq.com>
2021-08-08 20:41:28 +0800

    spring-boot-json

---
+++ spring-boot-json/src/test/java/cn/javanorth/springbootjson/SpringBootJsonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.javanorth.springbootjson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJsonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
